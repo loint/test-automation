bf1cd545860cf8011c714dbc7df48da792f9aa85
nguyentrungloi <loinguyentrung@gmail.com>
2014-05-28 18:28:12 +0700

    refactor code

--- config.js
+++ config.js
@@ -1,11 +1,58 @@
 // TEST CONFIGURATION
 config = { 
-    project :  'Stepping-Cloud',      
+    project :  'Stepping-Cloud',       
     test    :  'General Api',         
-    url     :  'http://192.168.2.134/sample/',
-    timeout :  99999999,   
-    browser :  'chrome',       
-    step    :  ['expect','click','set','key','submit'],
-    module  :  ['auth','resellers','customers','people','search']
+    timeout :  99999999, 
+    speed   :  50,    
+    browser :  'chrome',        
+    step    :  ['expect','click','set','key','submit'],   
+    ignore  :  [   
+        'sample>auth>customers',   
+        'sample>auth>people',
+        'sample>auth>search' 
+    ],       
+    flow :  {
+        sample: {
+            auth : {    
+                model:['account'],   
+                action: [   
+                    
+                ],
+                resellers: {                
+                    get_all: { 
+                        
+                    },
+                    get_one: {
+                    
+                    },
+                    create: {
+                    
+                    },
+                    update: {
+                        
+                    },
+                    partly_update: {
+                    
+                    },  
+                    delete: {
+                    
+                    }
+                },
+                customers : {
+                
+                },
+                people:{
+                 
+                },
+                search:{
+                
+                }
+            }
+        },
+        
+        app: {
+            
+        }
+    }
 } 
 
